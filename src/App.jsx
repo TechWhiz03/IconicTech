f0e04e27da05ebdaa20fc3bd5990881397cc9697
Rohila <asrohila03@gmail.com>
2023-10-06 23:10:02 +0530

    Added Pre Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import PreNav from './Components/PreNav'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <PreNav />
+    </div>
+  )
 }
 
 export default App
