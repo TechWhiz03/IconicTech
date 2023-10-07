db95fc1db2c3de710565112dd098574e1cd89b1d
Rohila <asrohila03@gmail.com>
2023-10-07 13:31:47 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import PreNav from './Components/PreNav'
+import Nav from './Components/Nav'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <PreNav />
-    </div>
+      <Nav />
+    </Router>
   )
 }
 
