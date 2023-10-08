165330adee5f362a780f06d0e16f45cdf3fd3829
Rohila <asrohila03@gmail.com>
2023-10-08 14:03:11 +0530

    Added Sliding Banner using Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import PreNav from './Components/PreNav'
 import Nav from './Components/Nav'
+import Slider from './Components/Slider'
 import { BrowserRouter as Router } from 'react-router-dom'
+import data from './Data/data.json'
 
 function App() {
   return (
     <Router>
       <PreNav />
       <Nav />
+      <Slider start={data.banner.start} />
     </Router>
   )
 }
