8016466769cfddf8db2d75d9e0daa34dd52149cc
Rohila <asrohila03@gmail.com>
2023-10-08 16:00:40 +0530

    Added Star Product Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './Components/Nav'
 import Slider from './Components/Slider'
 import Offers from './Components/Offers'
 import Heading from './Components/Heading'
+import StarProduct from './Components/StarProduct'
 import { BrowserRouter as Router } from 'react-router-dom'
 import data from './Data/data.json'
 
@@ -15,6 +16,7 @@ function App() {
       <Slider start={data.banner.start} />
       <Offers offer={data.offer} />
       <Heading text={'STAR PRODUCTS'} />
+      <StarProduct starProduct={data.starProduct} />
     </Router>
   )
 }
