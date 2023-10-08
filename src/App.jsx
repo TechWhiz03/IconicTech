14b9ef529139547e213b4d08c5576a10d785d29e
Rohila <asrohila03@gmail.com>
2023-10-08 15:18:53 +0530

    Added Offers Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import PreNav from './Components/PreNav'
 import Nav from './Components/Nav'
 import Slider from './Components/Slider'
+import Offers from './Components/Offers'
 import { BrowserRouter as Router } from 'react-router-dom'
 import data from './Data/data.json'
 
@@ -11,6 +12,7 @@ function App() {
       <PreNav />
       <Nav />
       <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
     </Router>
   )
 }
